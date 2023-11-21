6a64a2fe9bf6f29d50c154c8ca858d248ffb415a
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2023-11-21 22:38:56 +0200

    v2 21.11

--- 4task/5hw/script.js
+++ 4task/5hw/script.js
@@ -5,32 +5,27 @@ const y = prompt("друге число");
 const firstNum = +x;
 const secNum = +y;
 
-const pluss = 'add';
-const minus = 'sub';
-const multi = 'mult';
-const division = 'div';
-
 switch (ask) {
     //                                        ДІЯ ДОДАВАННЯ
-    case pluss:
+    case "add":
        let sum = firstNum + secNum;
        alert(`Результат дії add:
        ${firstNum} + ${secNum} = ${sum}`);
        break;
     //                                        ДІЯ ВІДНІМАННЯ   
-    case minus:
+    case 'sub':
         let riz = firstNum - secNum;
         alert(`Результат дії sub:
         ${firstNum} - ${secNum} = ${riz}`);
         break;
     //                                        ДІЯ МНОЖЕННЯ                                           
-    case multi:
+    case 'mult':
         let mnozh = firstNum * secNum;
         alert(`Результат дії mult:
         ${firstNum} * ${secNum} = ${mnozh}`);
         break;
     //                                        ДІЯ ДІЛЕННЯ 
-    case division:
+    case 'div':
         let dillenya = firstNum / secNum;
         alert(`Результат дії div:
         ${firstNum} / ${secNum} = ${dillenya}`);
