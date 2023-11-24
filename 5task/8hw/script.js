6f7ffa8aaf35997c7e5997934b99d23a88e32575
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2023-11-24 22:08:23 +0200

    8 дз (24.11)

---
+++ 5task/8hw/script.js
@@ -0,0 +1,11 @@
+const w = prompt('add num 1');
+const s = prompt('add num 2');
+const a = prompt('add num 3');
+
+const num1 = +w;
+const num2 = +s;
+const num3 = +a;
+
+const res = (num1 + num2 + num3) / 3;
+
+alert('Середнє арифметиче = ' + res);
