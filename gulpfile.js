6e5cb76b3b4eb1986ed4f8a60f79bdd789dbfd02
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-03-12 16:03:41 +0200

    16.03

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var rename = require("gulp-rename");
 var del = require("del");
 
 gulp.task("clean", function () {
-  return del.sync('dist')
+  return del.sync("dist");
 });
 
 gulp.task("sass-compile", function () {
@@ -35,37 +35,32 @@ gulp.task("browser-sync", function () {
 });
 
 gulp.task("scripts", function () {
-    return gulp.src([
-        "app/jquery/dist/jquery.min.js",
-        "app/magnific-popup/dist/jquery.magnific-popup.min.js",
+  return gulp
+    .src([
+      "app/jquery/dist/jquery.min.js",
+      "app/magnific-popup/dist/jquery.magnific-popup.min.js",
     ])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
-    
+    .pipe(concat("libs.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"));
 });
 
 gulp.task("css-libs", ["sass-compile"], function () {
-  return gulp.src('app/css/main.css')
+  return gulp
+    .src("app/css/main.css")
     .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'));
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("app/css"));
 });
 
 gulp.task("build", ["clean", "sass-compile", "scripts"], function () {
-  
-  var buildCss = gulp.src([
-    './app/css/main.css',
-    './app/css/libs.min.css'
-  ])
-  .pipe(gulp.dest('dist/css'));
+  var buildCss = gulp
+    .src(["./app/css/main.css", "./app/css/libs.min.css"])
+    .pipe(gulp.dest("dist/css"));
 
-  var buildFonts = gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'));
+  var buildFonts = gulp.src("app/fonts/**/*").pipe(gulp.dest("dist/fonts"));
 
-  var buildJs = gulp.src("app/js/**/*")
-    .pipe(gulp.dest("dist/js"));
+  var buildJs = gulp.src("app/js/**/*").pipe(gulp.dest("dist/js"));
 
-  var buildHtml = gulp.src("app/*.html")
-    .pipe(gulp.dest("dist"));
+  var buildHtml = gulp.src("app/*.html").pipe(gulp.dest("dist"));
 });
